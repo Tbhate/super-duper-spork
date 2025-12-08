6955432de88b2a92e14de1761e1bc88f377600bd
Tbhate <dmitriybut83@gmail.com>
2025-12-08 19:29:38 +0100

    translate bnt and fix style

---
+++ src/translate.js
@@ -0,0 +1,247 @@
+const translations = {
+  ru: {
+    "why.title": "Почему с нами выгоднее?",
+    "why.standard": "Стандартный проект в Польше стоит от 60 000 zł и занимает 6+ месяцев",
+    "why.withUs1": "С нами —",
+    "why.price": "от 26 000 zł",
+    "why.withUs2": "и в несколько раз быстрее",
+    "why.li1": "Минимум бюрократии, максимум скорости",
+    "why.li2": "Вы экономите деньги, время и нервы",
+    "why.li3": "От проекта до разрешения на строительство мы берём на себя",
+    "q.1": "Проект",
+    "q.2": "Что вы получаете",
+    "q.3": "Преимущества",
+    "q.4": "Услуги",
+    "q.5": "Стоимость",
+
+      "q.6": "Контакты",
+    "q.7": "Проект дома <br /> с согласованием  <br />— под ключ",
+    "q.8": "В 3 раза дешевле",
+    "q.9": "Строительство начинается не <br class='br-s' /> с фундамента — а с проекта. <br class='br-m' /> И если делать это <br class='br-s' />через стандартные компании, вы теряете<br class='br-m' /> месяцы <br class='br-s' />и десятки тысяч злотых.",
+    "q.10": "Оставить заявку",
+    "q.11": "Работаем <br /> по всей Польше",
+    "q.12": "Мы делаем это <br />быстрей и дешевле",
+    "q.13": "Что вы получаете:",
+    "q.14": "Готовый индивидуальный<br />  проект под вас",
+    "q.15": "Вы можете взять <br /> готовый или создадим новый проект, который <br /> идеально подходит вам, вашему<br />  участку и вашим желаниям.",
+    "q.16": "Полная адаптация под <br /> требования вашего UŻĄD",
+    "q.17": "Каждый город — свои требования. <br /> Мы приводим проект в полное <br /> соответствие, чтобы у ужонда <br /> не было вопросов.",
+    "q.18": "Мы сами согласовываем <br /> проект в ужонде",
+    "q.19": "Отправка, <br /> коммуникация, <br /> корректировки, ответы<br />  на запросы — всё на нас.",
+    "q.20": "Получение разрешения <br /> на строительство",
+    "q.21": "Вы получаете <br /> финальный документ <br /> и можете начинать <br /> строительство.",
+
+    "q.22": "Итог:",
+"q.23": "Вы получаете:",
+
+"q.24": "Полностью <br class='br-s' /><span>готовый <br class='br-s' />проект",
+"q.25": "Полностью <br class='br-s' /><span>согласованный</span><br /> в ужонде",
+"q.26": "<span>Разрешение</span> <br class='br-s' />на строительство",
+"q.27": " <span>Без очередей, <br class='br-s' />без бюрократии <br class='br-s' /> и без переплат<span>",
+
+"q.28": "Проект и согласование под <br class='br-s' />ключ —<br class='br-m' />просто, быстро <br class='br-s' />и недорого.",
+
+"q.29": "<span>Хочешь узнать <br class='br-m' /> стоимость <br class='br-s' /> </span>именно <br class='br-m' /> для твоего дома?",
+
+"q.30": "<span>Оставь заявку —</span> и мы <br />посчитаем <span class='span-calc'>в течение суток.</span>",
+
+"q.31": "Оставить заявку",
+
+"s.1": "Дополнительные услуги",
+    "s.2": "(по желанию):",
+
+    "s.3": "<span>Индивидуально</span><br />проект под вас",
+    "s.4": "<span>Доработка</span><br />готового проекта",
+    "s.5": "<span>Дизайн-проект</span><br />интерьера",
+    "s.6": "<span>Ландшафтный</span><br />дизайн",
+    "s.7": "<span>Проект</span> забора<br />и ограждений",
+    "s.8": "<span>Оборудование</span><br />объекта септиком",
+    "s.9": "Организация <span>скважины</span>",
+
+     "f.1": "Проект",
+    "f.2": "Преимущества",
+    "f.3": "Что вы получаете",
+    "f.4": "Услуги",
+    "f.5": "Стоимость",
+
+    "f.6": "Политика <br />конфиденциальности",
+    "f.7": "Политика <br />использования cookies",
+    "f.8": "<span>© AV Construction.</span> Все права защищены.",
+    "f.9": "Контакты",
+
+    "f.10": "Настройки cookies",
+    "f.11": "Условия предоставления услуг",
+    "f.12": "Информация об администраторе данных",
+  },
+
+  pl: {
+    "why.title": "Dlaczego my się opłaca?",
+    "why.standard": "Standardowy projekt w Polsce kosztuje od 60 000 zł i trwa 6+ miesięcy",
+    "why.withUs1": "Z nami —",
+    "why.price": "od 26 000 zł",
+    "why.withUs2": "i znacznie szybciej",
+    "why.li1": "Minimum biurokracji, maksimum szybkości",
+    "why.li2": "Oszczędzasz pieniądze, czas i nerwy",
+    "why.li3": "Od projektu po pozwolenie na budowę — wszystko bierzemy na siebie",
+    "q.1": "Projekt",
+    "q.2": "Co otrzymujesz",
+    "q.3": "Zalety",
+    "q.4": "Usługi",
+    "q.5": "Koszt",
+
+      "q.6": "Kontakt",
+    "q.7": "Projekt domu  <br />z uzgodnieniami <br /> — pod klucz",
+    "q.8": "3 razy taniej",
+    "q.9": "Budowa zaczyna się nie <br class='br-s' />od fundamentów — lecz od projektu.<br class='br-m' /> A jeśli robić to przez standardowe firmy, tracisz <br class='br-m' /> miesiące<br class='br-s' />  i dziesiątki tysięcy złotych.",
+    "q.10": "Zostawić zgłoszenie",
+    "q.11": "Działamy <br /> w całej Polsce",
+    "q.12": "Robimy to <br />szybciej i taniej",
+    "q.13": "Co otrzymujesz:",
+    "q.14": "Gotowy indywidualny <br /> projekt dla Ciebie",
+    "q.15": "Możesz wziąć gotowy lub <br /> stworzymy nowy projekt,<br />  idealny dla Ciebie, Twojej <br /> działki i Twoich potrzeb.",
+    "q.16": "Pełna adaptacja do <br /> wymagań Twojego UŻĄDU",
+    "q.17": "Każde miasto ma swoje wymagania.<br />  Dostosowujemy projekt tak,<br />  aby urząd nie miał<br />  żadnych pytań.",
+    "q.18": "Sami uzgadniamy <br /> projekt w urzędzie",
+    "q.19": "Wysyłka, <br /> kontakt, <br /> poprawki, odpowiedzi <br /> na zapytania — wszystko po naszej stronie.",
+    "q.20": "Uzyskanie pozwolenia<br />  na budowę",
+    "q.21": "Otrzymujesz <br /> końcowy dokument <br /> i możesz rozpoczynać <br /> budowę.",
+    
+    "q.22": "Podsum.",
+"q.23": "Otrzymujesz:",
+
+"q.24": "W pełni <br class='br-s' /><span>gotowy <br class='br-s' />projekt</span>",
+"q.25": "W pełni <br class='br-s' /><span>uzgodniony</span><br /> w urzędzie",
+"q.26": "<span>Pozwolenie</span> <br class='br-s' />na budowę",
+"q.27": "<span>Bez kolejek, <br class='br-s' />bez biurokracji <br class='br-s' /> i bez nadpłat</span>",
+
+"q.28": "Projekt i uzgodnienia pod <br class='br-s' />klucz —<br class='br-m' />prosto, szybko <br class='br-s' />i niedrogo.",
+
+"q.29": "<span>Chcesz poznać <br class='br-m' /> koszt <br class='br-s' /> </span>dokładnie <br class='br-m' /> dla twojego domu?",
+
+"q.30": "<span>Zostaw zgłoszenie —</span> a my <br />wycenimy <span class='span-calc'>w ciągu doby.</span>",
+
+"q.31": "Zostawić zgłoszenie",
+
+ "s.1": "Dodatkowe usługi",
+    "s.2": "(opcjonalnie):",
+
+    "s.3": "<span>Indywidualny</span><br />projekt dla Ciebie",
+    "s.4": "<span>Dostosowanie</span><br />gotowego projektu",
+    "s.5": "<span>Projekt wnętrza</span><br />i aranżacja",
+    "s.6": "<span>Projekt</span><br />krajobrazu",
+    "s.7": "<span>Projekt ogrodzenia</span><br />i płotów",
+    "s.8": "<span>Wyposażenie</span><br />obiektu w szambo",
+    "s.9": "Organizacja <span>studni</span>",
+     "f.1": "Projekt",
+    "f.2": "Korzyści",
+    "f.3": "Co otrzymujesz",
+    "f.4": "Usługi",
+    "f.5": "Koszt",
+
+    "f.6": "Polityka <br />prywatności",
+    "f.7": "Polityka <br />cookies",
+    "f.8": "<span>© AV Construction.</span> Wszelkie prawa zastrzeżone.",
+    "f.9": "Kontakt",
+
+    "f.10": "Ustawienia cookies",
+    "f.11": "Warunki świadczenia usług",
+    "f.12": "Informacje o administratorze danych",
+  },
+
+  uk: {
+    "why.title": "Чому з нами вигідніше?",
+    "why.standard": "Стандартний проєкт у Польщі коштує від 60 000 zł і триває 6+ місяців",
+    "why.withUs1": "З нами —",
+    "why.price": "від 26 000 zł",
+    "why.withUs2": "і в кілька разів швидше",
+    "why.li1": "Мінімум бюрократії, максимум швидкості",
+    "why.li2": "Ви економите гроші, час і нерви",
+    "why.li3": "Від проєкту до дозволу на будівництво — все беремо на себе",
+    "q.1": "Проект",
+    "q.2": "Що ви отримуєте",
+    "q.3": "Переваги",
+    "q.4": "Послуги",
+    "q.5": "Вартість",
+
+     "q.6": "Контакти",
+    "q.7": "Проєкт будинку  <br />з узгодженням <br />— під ключ",
+    "q.8": "У 3 рази дешевше",
+    "q.9": "Будівництво починається не<br class='br-s' />  з фундаменту — а з проєкту.<br class='br-m' />  Якщо робити це <br class='br-s' /> через стандартні компанії, ви втрачаєте <br class='br-m' /> місяці<br class='br-s' />  і десятки тисяч злотих.",
+    "q.10": "Залишити заявку",
+    "q.11": "Працюємо <br /> по всій Польщі",
+    "q.12": "Ми робимо це<br />швидше і дешевше",
+    "q.13": "Що ви отримуєте:",
+    "q.14": "Готовий індивідуальний<br /> проєкт під вас",
+    "q.15": "Ви можете взяти готовий або <br /> ми створимо новий проєкт,<br />  що ідеально підходить вам, вашій<br />  ділянці та вашим побажанням.",
+    "q.16": "Повна адаптація під<br />  вимоги вашого UŻĄD",
+    "q.17": "У кожного міста свої вимоги.<br />  Ми приводимо проєкт у повну <br /> відповідність, щоб у уженду <br /> не виникало питань.",
+    "q.18": "Ми самі узгоджуємо <br /> проєкт в уженді",
+    "q.19": "Відправлення,<br />  комунікація,<br />  правки, відповіді<br />  на запити — все на нас.",
+    "q.20": "Отримання дозволу <br /> на будівництво",
+    "q.21": "Ви отримуєте <br /> фінальний документ <br /> і можете починати<br /> будівництво.",
+    
+    "q.22": "Підсумок:",
+"q.23": "Ви отримуєте:",
+
+"q.24": "Повністю <br class='br-s' /><span>готовий <br class='br-s' />проєкт</span>",
+"q.25": "Повністю <br class='br-s' /><span>узгоджений</span><br /> в уженді",
+"q.26": "<span>Дозвіл</span> <br class='br-s' />на будівництво",
+"q.27": "<span>Без черг, <br class='br-s' />без бюрократії <br class='br-s' /> і без переплат</span>",
+
+"q.28": "Проєкт і узгодження під <br class='br-s' />ключ —<br class='br-m' />просто, швидко <br class='br-s' />і недорого.",
+
+"q.29": "<span>Хочеш дізнатися <br class='br-m' /> вартість <br class='br-s' /> </span>саме <br class='br-m' /> для твого дому?",
+
+"q.30": "<span>Залиш заявку —</span> і ми <br />прорахуємо <span class='span-calc'>протягом доби.</span>",
+
+"q.31": "Залишити заявку",
+
+"s.1": "Додаткові послуги",
+    "s.2": "(за бажанням):",
+
+    "s.3": "<span>Індивідуальний</span><br />проєкт під вас",
+    "s.4": "<span>Доопрацювання</span><br />готового проєкту",
+    "s.5": "<span>Дизайн-проєкт</span><br />інтерʼєру",
+    "s.6": "<span>Ландшафтний</span><br />дизайн",
+    "s.7": "<span>Проєкт</span> паркану<br />та огорож",
+    "s.8": "<span>Обладнання</span><br />обʼєкта септиком",
+    "s.9": "Організація <span>свердловини</span>",
+     "f.1": "Проєкт",
+    "f.2": "Переваги",
+    "f.3": "Що ви отримуєте",
+    "f.4": "Послуги",
+    "f.5": "Вартість",
+
+    "f.6": "Політика <br />конфіденційності",
+    "f.7": "Політика <br />використання cookies",
+    "f.8": "<span>© AV Construction.</span> Усі права захищені.",
+    "f.9": "Контакти",
+
+    "f.10": "Налаштування cookies",
+    "f.11": "Умови надання послуг",
+    "f.12": "Інформація про адміністратора даних",
+  },
+
+};
+
+function translatePage(lang) {
+  document.querySelectorAll("[data-i18n]").forEach(el => {
+    const key = el.getAttribute("data-i18n");
+    if (translations[lang] && translations[lang][key]) {
+      el.innerHTML = translations[lang][key];
+    }
+  });
+}
+document.querySelectorAll(".transl-btn").forEach(btn => {
+  btn.addEventListener("click", () => {
+    const lang = btn.dataset.lang;
+    translatePage(lang);
+
+    // сохраняем выбранный язык
+    localStorage.setItem("language", lang);
+  });
+});
+
+// загрузка языка при обновлении страницы
+const savedLang = localStorage.getItem("language");
+if (savedLang) translatePage(savedLang);
