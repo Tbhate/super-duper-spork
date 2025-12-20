17108cf3204a4123716393db3767e35c2fceb095
Tbhate <dmitriybut83@gmail.com>
2025-12-20 15:41:31 +0100

    fix bug

--- src/gallery.js
+++ src/gallery.js
@@ -18,3 +18,38 @@ prevBtn.addEventListener('click', () => {
     behavior: 'smooth'
   });
 });
+
+
+// open photo 
+
+document.addEventListener('DOMContentLoaded', () => {
+  const modal = document.getElementById('imgModal');
+  const modalImg = document.getElementById('imgModalContent');
+  const closeBtn = document.querySelector('.img-modal-close');
+
+  document.querySelectorAll('.gallery-item img').forEach(img => {
+    img.addEventListener('click', () => {
+      modal.style.display = 'flex';
+      modalImg.src = img.src;
+
+      // даём браузеру отрисовать display:flex
+      setTimeout(() => modal.classList.add('active'), 10);
+
+      document.body.style.overflow = 'hidden';
+    });
+  });
+
+  function closeModal() {
+    modal.classList.remove('active');
+    setTimeout(() => {
+      modal.style.display = 'none';
+      modalImg.src = '';
+      document.body.style.overflow = '';
+    }, 300);
+  }
+
+  closeBtn.addEventListener('click', closeModal);
+  modal.addEventListener('click', e => {
+    if (e.target === modal) closeModal();
+  });
+});
